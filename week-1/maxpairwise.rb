ea8fde78bda3d4f9b30c3a1c30361d6ce144783d
amingilani <aminshahgilani@gmail.com>
2016-07-01 15:03:55 +0500

    Use an alternative algorithm

--- week-1/maxpairwise.rb
+++ week-1/maxpairwise.rb
@@ -2,6 +2,42 @@
 
 # given a sequence of numbers, find the maximum product when multiplying two
 # of them
+require 'net/http'
 
-array = $stdin.read.split[1..-1]
-puts array.sort.map(&:to_i).last(2).reduce(&:*)
+array = $stdin.read.split
+
+garray = array[1..-1].map(&:to_i)
+
+# require 'set'
+#
+# def rand_n(n, max)
+#     randoms = Set.new
+#     loop do
+#         randoms << rand(max)
+#         return randoms.to_a if randoms.size >= n
+#     end
+# end
+
+# def sort_array m
+#   m.sort.map(&:to_i).last(2).reduce(&:*)
+# end
+
+def max_value m
+    a = m.max
+    b = m.count(m.max) > 1 ? a : (m - [m.max]).max
+    puts "b is nil" if b.nil?
+    puts "a is nil" if a.nil?
+    a * b
+end
+
+# loop do
+#   m = rand_n(rand(100) + 2, 10000000000000000000000000000000000000000000000)
+#   puts m
+#   sort = sort_array m
+#   max = max_value m
+#   puts "sort: #{sort}"
+#   puts "max: #{max}"
+#   break if sort != max
+# end
+
+puts max_value array
