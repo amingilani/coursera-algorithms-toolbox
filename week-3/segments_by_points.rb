be97d785bd38059e97fa0c624586190e1e33d317
amingilani <aminshahgilani@gmail.com>
2016-07-03 00:25:40 +0500

    First failed algorithm

--- week-3/segments_by_points.rb
+++ week-3/segments_by_points.rb
@@ -6,18 +6,45 @@
 # the minimum size such that for any segment [ai,bi] there is a point x in set X
 # such that ai <= x <= bi.
 
-inputs = $stdin.read
-               .split("\n")
-               .map(&:chomp)
-               .map(&:split)
-               .map { |e| e.map(&:to_i) }
-test_input_1 = [[3], [1, 3], [2, 5], [3, 6]]
-
-def points(input)
+def points_batao(input)
   length = input.shift[0]
-  lines = input.sort_by { |e| e[0] }
-  smallest_point = lines.map { |e| e[0] }.minimum
-  largest_point = lines.map { |e| e[0] }.maximum
+  lines = input.sort_by { |e| e[0] }.map { |e| (e[0]..e[1]).to_a }
+  puts "lines are #{lines}"
   points = []
-
+  tmp = []
+  lines.each_with_index do |m, i|
+    puts "going over line #{m}"
+    if tmp.empty?
+      tmp += m
+      puts "tmp is empty, adding #{tmp}"
+      next
+    end
+    common_points = tmp & m
+    puts "common_points: #{common_points}"
+    if common_points.empty?
+      points << tmp.first
+      tmp = m
+      puts "Adding #{tmp.first} to points: #{points}"
+      puts "Adding #{m} to tmp"
+      next
+    end
+    if i == length - 1
+      points << tmp.last
+      puts "Adding #{tmp.first} to points: #{points}"
+      next
+    end
+    tmp = common_points
+  end
+  points
 end
+
+# inputs = $stdin.read
+#                .split("\n")
+#                .map(&:chomp)
+#                .map(&:split)
+#                .map { |e| e.map(&:to_i) }
+
+test_input_1 = [[3], [1, 3], [2, 5], [3, 6]]
+test_input_2 = [[4], [4, 7], [1, 3], [2, 5], [5, 6]]
+
+puts points_batao test_input_2
